d524e48994e8a3253b38d2d9715e6a1417c38890
lemon <lemon@fint.finance>
2024-07-11 15:09:55 +0700

    Remove unused type and console log

--- src/utils/responses/streamMessage.response.ts
+++ src/utils/responses/streamMessage.response.ts
@@ -1,44 +1,37 @@
 import { Readable } from 'stream';
 import type { Response } from 'express';
-import { AIMessageChunk } from '@langchain/core/messages';
-import { IterableReadableStream } from '@langchain/core/utils/stream';
-
-
 
 function streamMessage(res: Response, stream: any) {
-
-    const readableStream = new Readable({
-        read() { }
-    });
-    // Push data from the stream to the readable stream
-    //basic-chat no longer work with this method
-    (async () => {
-        try {
-            for await (const event of stream) {
-                if (event.event == "on_llm_stream") {
-                    readableStream.push(event.data.chunk.text);
-                }
-            }
-            readableStream.push(null); // Signal the end of the stream
-        } catch (error) {
-            readableStream.destroy(error); // Destroy the stream on error
+  const readableStream = new Readable({
+    read() {},
+  });
+  // Push data from the stream to the readable stream
+  //basic-chat no longer work with this method
+  (async () => {
+    try {
+      for await (const event of stream) {
+        if (event.event == 'on_llm_stream') {
+          readableStream.push(event.data.chunk.text);
         }
-    })();
+      }
+      readableStream.push(null); // Signal the end of the stream
+    } catch (error) {
+      readableStream.destroy(error); // Destroy the stream on error
+    }
+  })();
 
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    readableStream.pipe(res);
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  readableStream.pipe(res);
 
-    // Attach error event listener to the readable stream
-    readableStream.on('error', (error) => {
-        console.error('Error occurred while streaming:', error);
-        res.end(); // End the response to prevent hanging
-    });
+  // Attach error event listener to the readable stream
+  readableStream.on('error', (error) => {
+    console.error('Error occurred while streaming:', error);
+    res.end(); // End the response to prevent hanging
+  });
 
-    // Attach end event listener to the readable stream
-    readableStream.on('end', () => {
-        // console.log("end of stream");
-        res.end(); // End the response when the stream ends
-    });
-
+  // Attach end event listener to the readable stream
+  readableStream.on('end', () => {
+    res.end(); // End the response when the stream ends
+  });
 }
 export default streamMessage;
