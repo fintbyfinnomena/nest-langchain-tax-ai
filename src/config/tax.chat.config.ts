05fffc3e17071eaf119c4aef55b040f000eff62b
Kawin Tiraborisute <kawin.ac125@gmail.com>
2024-08-26 16:09:46 +0800

    Added default value for ttl and question limit

--- src/config/tax.chat.config.ts
+++ src/config/tax.chat.config.ts
@@ -16,8 +16,8 @@ export const getConfig = () => {
 
     mongoConnString: process.env.MONGO_CONNECTION_STRING,
 
-    taxQuestionTtlHrs: Number(process.env.TAX_QUESTION_TTL_HRS),
-    taxQuestionLimit: Number(process.env.TAX_QUESTION_LIMIT),
+    taxQuestionTtlHrs: Number(process.env.TAX_QUESTION_TTL_HRS) || 12,
+    taxQuestionLimit: Number(process.env.TAX_QUESTION_LIMIT) || 20,
 
     fundQuote: {
       baseUrl: 'https://www.finnomena.com/fund/',
